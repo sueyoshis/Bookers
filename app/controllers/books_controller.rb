536344fb1dac33236ce537343ca7de89e4e2c09e
sueyoshis <99736235+sueyoshis@users.noreply.github.com>
2022-03-07 10:14:52 +0000

    [Add]削除機能の追加、一覧画面の遷移先設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new (book_params)
     book.save
-    redirect_to book_path(book.id)
+      redirect_to book_path(book.id)
   end
 
   def show
@@ -23,6 +23,12 @@ class BooksController < ApplicationController
     book = Book.find (params[:id])
     book.update (book_params)
     redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find (params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
