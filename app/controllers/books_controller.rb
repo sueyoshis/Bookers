f06b7109d7e0d820f7e8e36311e808cf4f601c7c
sueyoshis <99736235+sueyoshis@users.noreply.github.com>
2022-03-07 12:13:42 +0000

    [Add]投稿・更新時のサクセスメッセージ表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,8 +7,8 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new (book_params)
-    if
-      @book.save
+    if @book.save
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
       @books = Book.all
@@ -26,8 +26,8 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find (params[:id])
-    if
-      @book.update (book_params)
+    if @book.update (book_params)
+      flash[:notice] = "Book was successfully updated."
       redirect_to book_path(@book.id)
     else
       render :edit
